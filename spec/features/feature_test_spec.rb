3595da162cde91d0aa513627f36e1424b5c24b8b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-15 15:13:04 +0100

    second feature test written

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -1,6 +1,14 @@
 feature Makers_bnb do 
-  scenario 'it loads with Makers Bnb on the home page' do
+  scenario 'It loads with Makers Bnb on the home page' do
     visit('/')
     expect(page).to have_content('Makers BnB')
+  end
+
+  feature('View Listings') do
+    scenario 'It has a separate page to view currently listed properties' do
+      visit('/')
+      click_button('View Properties')
+      expect(page).to have_content('Available Properties')
+    end
   end
 end
