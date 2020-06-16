0c3ffdae7671b064372c196886923caf3c74b22f
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-16 15:13:29 +0100

    button added to request stay at listed properties

--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -54,4 +54,15 @@ feature Makers_bnb do
       expect(page).to have_content('This is a description')
     end
   end
+
+  feature('Request a stay') do
+    scenario 'each property renders with a button to request a stay' do
+    visit('/list_space') 
+      fill_in 'name', with: 'party house'
+      fill_in 'price', with: '80'
+      fill_in 'description', with: 'This is a description'
+      click_button('List Property')
+      expect(page).to have_button('Request a stay')
+    end
+  end
 end
