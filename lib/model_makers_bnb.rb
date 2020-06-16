e507c3fd43266e93bf3adea9b136c1f4d545d604
Tristan Langford <tristanlangford@hotmail.com>
2020-06-16 16:45:05 +0100

    testing password encryption

--- lib/model_makers_bnb.rb
+++ lib/model_makers_bnb.rb
@@ -13,7 +13,6 @@ class Model_Makers_bnb
 
   def self.get_users
       users = Database_connection.query("SELECT * FROM users")
-
       users.map { |user| User.new( user['user_id'], user['first_name'], user["last_name"], user["email"], user["password"]) }
   end
 
@@ -27,4 +26,5 @@ class Model_Makers_bnb
   def self.encrypt_password(password)
     BCrypt::Password.create(password)
   end
+  
 end
