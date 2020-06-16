03877ffc8af17416587f61ece589e8a67c319052
Tristan Langford <tristanlangford@hotmail.com>
2020-06-16 10:36:34 +0100

    created signup with first name

--- spec/features/sign_up_in_spec.rb
+++ spec/features/sign_up_in_spec.rb
@@ -5,5 +5,10 @@ feature 'signup' do
         expect(page).to have_content("Email:")
     end
 
+    scenario 'it allows the user to enter their email address' do 
+        visit('/signup')
+        expect(page).to have_content("First name:")
+    end
+
 
 end
