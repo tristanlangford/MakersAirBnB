8dd0bcf2590ec939a44efd64bd43fe7cbe5d6181
Tristan Langford <tristanlangford@hotmail.com>
2020-06-16 10:38:14 +0100

    created signup with last name

--- spec/features/sign_up_in_spec.rb
+++ spec/features/sign_up_in_spec.rb
@@ -5,10 +5,14 @@ feature 'signup' do
         expect(page).to have_content("Email:")
     end
 
-    scenario 'it allows the user to enter their email address' do 
+    scenario 'it allows the user to enter their first name' do 
         visit('/signup')
         expect(page).to have_content("First name:")
     end
 
+    scenario 'it allows the user to enter their last name' do 
+        visit('/signup')
+        expect(page).to have_content("Last name:")
+    end
 
 end
