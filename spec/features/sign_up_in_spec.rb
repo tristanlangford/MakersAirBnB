257f5ef64376e3ccd41dafc3b3bf7d0c7084d46a
Nikita <latinisnotdead07@gmail.com>
2020-06-16 10:52:24 +0100

    creates email field in form

--- spec/features/sign_up_in_spec.rb
+++ spec/features/sign_up_in_spec.rb
@@ -1,26 +1,27 @@
-feature 'signup' do 
+feature 'signup' do
 
-    scenario 'it allows the user to enter their email address' do 
+    scenario 'it allows the user to enter their email address' do
         visit('/signup')
+        fill_in('email', with: 'some_email@hotmail.com')
         expect(page).to have_content("Email:")
     end
 
-    scenario 'it allows the user to enter their first name' do 
+    scenario 'it allows the user to enter their first name' do
         visit('/signup')
         expect(page).to have_content("First name:")
     end
 
-    scenario 'it allows the user to enter their last name' do 
+    scenario 'it allows the user to enter their last name' do
         visit('/signup')
         expect(page).to have_content("Last name:")
     end
 
-    scenario 'it allows the user to enter their password' do 
+    scenario 'it allows the user to enter their password' do
         visit('/signup')
         expect(page).to have_content("Password:")
     end
 
-    scenario 'it allows the user to confirm their password' do 
+    scenario 'it allows the user to confirm their password' do
         visit('/signup')
         expect(page).to have_content("Confirm Password:")
     end
