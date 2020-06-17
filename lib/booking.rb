79864d1f1430380b0910396225fac569855d2575
Tristan Langford <tristanlangford@hotmail.com>
2020-06-17 15:42:16 +0100

    added prop_id to booking

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,10 +1,11 @@
 class Booking
 
-    attr_reader :start_date, :end_date
+    attr_reader :start_date, :end_date, :prop_id
 
-    def initialize(start_date, end_date)
+    def initialize(start_date, end_date, prop_id)
         @start_date = start_date
         @end_date = end_date
+        @prop_id = prop_id
     end
 
 end
--- spec/units/booking_spec.rb
+++ spec/units/booking_spec.rb
@@ -2,7 +2,7 @@ require 'booking'
 
 describe Booking do 
 
-    subject { Booking.new('01/01/20', '10/01/20') }
+    subject { Booking.new('01/01/20', '10/01/20', '1') }
 
     it 'should have a start_date' do 
         expect(subject.start_date).to eq('01/01/20')
@@ -10,6 +10,10 @@ describe Booking do
 
     it 'should have an end_date' do 
         expect(subject.end_date).to eq('10/01/20')
+    end
+
+    it 'should have a prop_id' do 
+        expect(subject.prop_id).to eq('1')
     end
 
 end
