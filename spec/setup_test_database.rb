56df04251c43689fb0263609cf51680758b9eafa
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-17 16:18:42 +0100

    test added for listing bookings

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,8 +3,10 @@ def setup_test_database
 
     connection = PG.connect :dbname => 'makers_bnb_test'
 
-    connection.exec("TRUNCATE properties;")
+    connection.exec("TRUNCATE properties CASCADE;")
 
-    connection.exec("TRUNCATE users;")
+    connection.exec("TRUNCATE users CASCADE;")
+
+    connection.exec("TRUNCATE bookings CASCADE;")
 
 end
--- spec/units/booking_spec.rb
+++ spec/units/booking_spec.rb
@@ -28,4 +28,24 @@ describe Booking do
         expect(subject.confirmation).to eq('n')
     end
 
+    describe 'List_bookings' do
+        it ' should return a Booking instance with all booking info' do
+            Database_connection.query(("INSERT INTO users (user_id, email, first_name, last_name,
+                password) VALUES (1, 'some_email@hotmail.com', 'Tristan', 'Langford', 'password');"))
+
+            Database_connection.query(("INSERT INTO properties VALUES (1, 'house 1', 'top house' ,'100' );"))
+
+            Database_connection.query("INSERT INTO bookings(booking_id, start_date, end_date, comments, booker_id, property_id, confirmation)
+            VALUES ('1', '01/01/20', '10/01/20', 'nice place!', '1', '1', 'n')")
+            
+            property = Booking.List_bookings
+            expect(property.first.start_date).to eq('01/01/20')
+            expect(property.first.end_date).to eq('10/01/20')
+            expect(property.first.comment).to eq('nice place!')
+            expect(property.first.prop_id).to eq('1')
+            expect(property.first.booker_id).to eq('1')
+            expect(property.first.confirmation).to eq('n')
+        end
+    end
+
 end
