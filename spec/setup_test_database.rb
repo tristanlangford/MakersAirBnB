d568c6f19edab4d7a977d4038bb4ebd3a2392b33
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-06-18 14:21:24 +0100

    updated db/migrations for new table + truncate

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -10,11 +10,12 @@ def setup_test_database
 
     connection.exec("TRUNCATE bookings CASCADE;")
 
+    connection.exec("TRUNCATE available_dates CASCADE;")
+
     password = BCrypt::Password.create('password')
 
     connection.exec(("INSERT INTO users (email, first_name, last_name,
               password, user_id) VALUES ('some_email@hotmail.com', 'Tristan', 'Langford', '#{password}', '1');"))
 
-
-
+    
 end
